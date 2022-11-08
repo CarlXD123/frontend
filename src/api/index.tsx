8cd5f7c952e08942ebef9e2bc3ce6fbe947f928b
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2022-11-08 01:31:50 -0500

    Update index.tsx

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import { apiFetch } from "./apiFetch";
 
 const PORT_BACKEND = process.env.REACT_APP_PORT_BACKEND || "3000";
-const URL_BACKEND = process.env.REACT_APP_API_URL_BACKEND || "http://localhost";
+const URL_BACKEND = process.env.REACT_APP_API_URL_BACKEND || "https://backend78.herokuapp.com/";
 
 export const API_URL_BACKEND = `${URL_BACKEND}:${PORT_BACKEND}/api/`;
 
